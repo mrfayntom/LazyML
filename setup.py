3356018352ba3716a0aae8c09a661c0e94b0fbca
Mr Phantom <mrfayntom@gmail.com>
2025-06-16 10:23:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="lazy-ml",
+    name="lazytrainer",
     version="0.1.0",
     author="Mr. Phantom",
     author_email="mrfayntom@gmail.com",
